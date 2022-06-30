81427e42abf5a6abe08221d87bf097f63794d519
Htanto <35892615+Htanto@users.noreply.github.com>
2022-06-30 15:52:09 +0700

    variable change in instance name

--- variables.tf
+++ variables.tf
@@ -10,6 +10,6 @@ variable "instance_type" {
 
 variable "instance_name" {
   description = "EC2 instance name"
-  default     = "Provisioned by Hendra-Terraform"
+  default     = "Provisioned by Hendra123-Terraform"
 }
 
